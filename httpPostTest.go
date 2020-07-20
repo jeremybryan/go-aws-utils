1ecb352c07f8ed52822d44e7179d74380bf8b606
jbryan <jbryan@tapestrytech.com>
2020-07-20 15:07:35 -0400

    Working through the post

--- httpPostTest.go
+++ httpPostTest.go
@@ -13,10 +13,11 @@ func main ()  {
 	if *endpoint != "" {
 		resp, err := http.Post(*endpoint, "application/json", nil)
 		if err != nil {
-			fmt.Errorf("Error completing HTTP POST %s", err.Error())
-		}
-		defer resp.Body.Close()
-		fmt.Printf("HTTP Post to %s has been completedl Response status: %s", endpoint, resp.Status)
+		   fmt.Errorf("Error completing HTTP POST %s\n", err.Error())
+		} else {
+                   defer resp.Body.Close()	
+		   fmt.Printf("HTTP Post to %s has been completed. Response status: %s", *endpoint, resp.Status)
+                }
 	} else {
 		fmt.Print("No endpoint has been defined. Exiting")
 	}
